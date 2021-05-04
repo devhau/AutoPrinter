48040026affb73e73ede9d918e6155815487eb48
Nguyen Van Hau <nguyenvanhaudev@gmail.com>
2021-05-04 10:31:00 +0700

    Init
    Init

---
+++ src/AutoPrinter/frmMain.Designer.cs
@@ -0,0 +1,325 @@
+﻿
+namespace AutoPrinter
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnLoadPrinterList = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtPass = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbPrinterList = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMails = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnLoadPrinterList);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.txtPass);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cbPrinterList);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(367, 165);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thiết lập";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(80, 136);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Lưu cấu hình";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(184, 136);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 25);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Bắt đầu chạy";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 46);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Mật khẩu";
+            // 
+            // btnLoadPrinterList
+            // 
+            this.btnLoadPrinterList.Location = new System.Drawing.Point(302, 67);
+            this.btnLoadPrinterList.Margin = new System.Windows.Forms.Padding(2);
+            this.btnLoadPrinterList.Name = "btnLoadPrinterList";
+            this.btnLoadPrinterList.Size = new System.Drawing.Size(47, 23);
+            this.btnLoadPrinterList.TabIndex = 6;
+            this.btnLoadPrinterList.Text = "Load";
+            this.btnLoadPrinterList.UseVisualStyleBackColor = true;
+            this.btnLoadPrinterList.Click += new System.EventHandler(this.BtnLoadPrinterList_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(11, 94);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(91, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Thời gian kiểm tra";
+            // 
+            // txtPass
+            // 
+            this.txtPass.Location = new System.Drawing.Point(109, 43);
+            this.txtPass.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPass.Name = "txtPass";
+            this.txtPass.PasswordChar = '*';
+            this.txtPass.Size = new System.Drawing.Size(240, 20);
+            this.txtPass.TabIndex = 2;
+            this.txtPass.Enter += new System.EventHandler(this.txtPass_Enter);
+            this.txtPass.Leave += new System.EventHandler(this.txtPass_Leave);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(192, 94);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Phút / lần";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Email";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(109, 92);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(79, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(109, 19);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(240, 20);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 70);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Máy In";
+            // 
+            // cbPrinterList
+            // 
+            this.cbPrinterList.FormattingEnabled = true;
+            this.cbPrinterList.Location = new System.Drawing.Point(109, 67);
+            this.cbPrinterList.Margin = new System.Windows.Forms.Padding(2);
+            this.cbPrinterList.Name = "cbPrinterList";
+            this.cbPrinterList.Size = new System.Drawing.Size(189, 21);
+            this.cbPrinterList.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.txtMails);
+            this.groupBox2.Location = new System.Drawing.Point(383, 11);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(340, 324);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Mail Đăng ký in tự động";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(5, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Lưu";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMails
+            // 
+            this.txtMails.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.txtMails.Location = new System.Drawing.Point(2, 42);
+            this.txtMails.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMails.Multiline = true;
+            this.txtMails.Name = "txtMails";
+            this.txtMails.Size = new System.Drawing.Size(334, 280);
+            this.txtMails.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.textBox2);
+            this.groupBox4.Location = new System.Drawing.Point(13, 176);
+            this.groupBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox4.Size = new System.Drawing.Size(366, 161);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Template Mail";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textBox2.Location = new System.Drawing.Point(2, 15);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(362, 144);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 9;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(732, 346);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(748, 385);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(748, 385);
+            this.Name = "frmMain";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Auto Mail Printer";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPass;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtMails;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbPrinterList;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnLoadPrinterList;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ src/AutoPrinter/frmMain.cs
@@ -0,0 +1,302 @@
+﻿using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace AutoPrinter
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar = '\0';
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            txtPass.PasswordChar = '*';
+        }
+
+        private void BtnLoadPrinterList_Click(object sender, EventArgs e)
+        {
+            cbPrinterList.Items.Clear();
+            foreach (string item in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                cbPrinterList.Items.Add(item);
+            }
+            if(cbPrinterList.Items.Count>0)
+                cbPrinterList.SelectedIndex = 1;
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            btnLoadPrinterList.PerformClick();
+            try
+            {
+                var settings = File.ReadAllLines("setting.txt");
+                txtEmail.Text = settings[0];
+                txtPass.Text = settings[1];
+                cbPrinterList.Text = settings[2];
+                numericUpDown1.Text = settings[3];
+            }
+            catch
+            {
+
+            }
+            
+            try
+            {
+
+                txtMails.Lines = File.ReadAllLines("mail.txt");
+            }
+            catch
+            {
+
+            }
+        }
+        void CheckMailPrinter()
+        {
+            try
+            {
+                using (var client = new ImapClient())
+                {
+                    client.Connect("imap.gmail.com", 993, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+
+                    client.Authenticate(txtEmail.Text, txtPass.Text);
+                    client.Inbox.Open(FolderAccess.ReadWrite);
+                    var FolderTrash = client.GetFolder(SpecialFolder.Trash);
+                    var mail = client.Inbox.Search(SearchQuery.All);
+                    
+                    foreach (var ids in mail)
+                    {
+                        Application.DoEvents();
+                        var message = client.Inbox.GetMessage(ids);
+                        if ((txtMails.Lines.Length>0 && !txtMails.Lines.Contains(message.Sender.Address))||!message.Subject.StartsWith("[AutoMailPrinter]")) continue;
+                        label6.Text = "Thực hiện in mail: " + message.Subject;
+                        client.Inbox.MoveTo(ids, FolderTrash);
+                        if (message.Attachments.Count() > 0)
+                        {
+                            foreach (var item in message.Attachments)
+                            {
+                                string pathFile = DateTime.Now.ToString("yyyy-MM-dd-HHmmss") + "-" + item.ContentDisposition.FileName;
+                                using (var stream = File.Create(pathFile))
+                                {
+                                    if (item is MessagePart)
+                                    {
+                                        var part = (MessagePart)item;
+
+                                        part.Message.WriteTo(stream);
+                                    }
+                                    else
+                                    {
+                                        var part = (MimePart)item;
+
+                                        part.Content.DecodeTo(stream);
+                                    }
+                                }
+                                Application.DoEvents();
+                                PrintPdf(pathFile, message.GetTextBody(MimeKit.Text.TextFormat.Text)); 
+                                Application.DoEvents();
+                                try
+                                {
+                                    File.Delete(pathFile);
+                                }
+                                catch
+                                {
+
+                                }
+                            }
+                        }
+                        Console.WriteLine("Subject: {0}", message.Subject);
+                    }
+
+                    client.Disconnect(true);
+                }
+            }catch(Exception ex)
+            {
+                File.WriteAllText("err.txt", ex.Message);
+            }
+            
+            int time = (int)numericUpDown1.Value;
+            time = time * 60;
+            while (true)
+            {
+                Application.DoEvents();
+                if (!flgStart) return;
+                    time--;
+                if (time <= 0) break;
+                Thread.Sleep(1000);
+                label6.Text = "Chờ "+time+" giây";
+            }
+            CheckMailPrinter();
+        }
+        private void PrintPdf(string pdfPath,string config)
+        {
+            using (PdfDocument document = PdfDocument.Load(pdfPath))
+            {
+                using(PrintDocument printDocument = document.CreatePrintDocument())
+                {
+                    printDocument.PrinterSettings = GetPrinterSettings(config);
+                    printDocument.Print();
+                }
+            }
+        }
+        private PrinterSettings GetPrinterSettings(string configs)
+        {
+            var PrinterSettings = new PrinterSettings();
+            PrinterSettings.PrinterName = cbPrinterList.Text;
+            var configList = configs.Split('\n');
+           foreach(var item in configList)
+            {
+                var value = item.Substring(item.IndexOf(":") + 1);
+                //Trang: 1 - 10 (Full)
+                if (item.IndexOf("Trang:") >-1)
+                {
+                    if (value.IndexOf('-') > -1)
+                    {
+                        var value_start = value.Substring(0,value.IndexOf("-"));
+                        var value_end = value.Substring(value.IndexOf("-") + 1);
+                        PrinterSettings.FromPage= ParseInt(value_start, 0);
+                        PrinterSettings.ToPage = ParseInt(value_end, 0);
+                    }
+                }
+                //Số Lượng: 5(Mặc định 1)
+                if (item.IndexOf("Số Lượng:") > -1)
+                {
+                    PrinterSettings.Copies = (short)ParseInt(value, 1);
+                }
+                //Kiểu: 1 (1-Dọc, 2- Ngang)
+                if (item.IndexOf("Kiểu:") > -1)
+                {
+                    //var varInt = (short)ParseInt(value, 1); ;
+                    //PrinterSettings.Collate = varInt == 1;
+
+                }
+                //Loại: 1(1 - 1 mặt, 2 - 2 mặt dọc, 3 - 2 mặt ngang)
+                if (item.IndexOf("Loại:") > -1)
+                {
+                    var varInt = (short)ParseInt(value, 1); ;
+                    if (varInt == 2)
+                    {
+                        PrinterSettings.Duplex = Duplex.Vertical;
+                    }
+                    if (varInt == 3)
+                    {
+                        PrinterSettings.Duplex = Duplex.Horizontal;
+                    }
+                }
+            }
+            return PrinterSettings;
+        }
+        private int ParseInt(string num,int @default=0)
+        {
+            try
+            {
+                return int.Parse(num.Trim());
+            }catch
+            {
+                return @default;
+            }
+        }
+        private bool flgStart = false;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng nhập email");
+                return;
+            }
+            if (txtPass.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng nhập pass của email");
+                return;
+            }
+            if (cbPrinterList.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng chọn máy in");
+                return;
+            }
+            groupBox2.Enabled = flgStart;
+            txtEmail.Enabled = flgStart;
+            txtPass.Enabled = flgStart;
+            cbPrinterList.Enabled = flgStart;
+            numericUpDown1.Enabled = flgStart;
+            btnLoadPrinterList.Enabled = flgStart;
+            Application.DoEvents();
+            if (flgStart)
+            {
+                label6.Text = "";
+                btnStart.Text = "Bắt đầu chạy";
+                Application.DoEvents();
+                flgStart = false;
+            }
+            else
+            {
+                label6.Text = "";
+                flgStart = true;
+                btnStart.Text = "Dừng lại";
+                label6.Text = "Bắt đầu đọc mail"; 
+                Application.DoEvents();
+                CheckMailPrinter();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtEmail.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng nhập email");
+                return;
+            }
+            if (txtPass.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng nhập pass của email");
+                return;
+            }
+            if (cbPrinterList.Text.Trim() == "")
+            {
+                MessageBox.Show("Vui lòng chọn máy in");
+                return;
+            }
+
+            List<string> settings = new List<string>();
+            settings.Add(txtEmail.Text);
+            settings.Add(txtPass.Text);
+            settings.Add(cbPrinterList.Text);
+            settings.Add(numericUpDown1.Text);
+            File.WriteAllLines("setting.txt", settings);
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                File.WriteAllLines("mail.txt", txtMails.Lines);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
